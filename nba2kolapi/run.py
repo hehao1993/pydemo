d3a73db6f267d2bd528b2b187daf10671e01d9a3
hehao1993 <347428365@qq.com>
2019-10-08 14:41:37 +0800

    add nba2kol2api, numpy_test

--- nba2kolapi/run.py
+++ nba2kolapi/run.py
@@ -1,16 +1,28 @@
+import json
 import requests
 from aiohttp import web
+import urllib3
+urllib3.disable_warnings()
 
 routes = web.RouteTableDef()
 
 
 @routes.get('/rosterList')
 async def hello(request):
-    print(request.query.get('page'))
-    # url = "https://nba2k2app.game.qq.com/game/trade/rosterList"
-    # json = requests.get(url, params=paras, verify=False).json()
-    # print(json.dumps(json, indent=4, ensure_ascii=False))
-    return web.json_response({'a': 1})
+    query = dict(request.query)
+    # print(query)
+    # print(request.query_string)
+    paras = {
+        'page': query['page'],
+        'lowPrice': query['lowPrice'],
+        'highPrice': query['highPrice'],
+        'collapseAll': query['collapseAll'],
+        'orderBy': query['orderBy'],
+        'orientation': query['orientation']
+    }
+    url = "https://nba2k2app.game.qq.com/game/trade/rosterList"
+    json_result = requests.get(url, params=paras, verify=False).json()
+    return web.json_response(json_result)
 
 app = web.Application()
 app.add_routes(routes)
--- nba2kolapi/test.py
+++ nba2kolapi/test.py
@@ -11,15 +11,16 @@ paras = {
     'highPrice': 0,
     'collapseAll': False,
     'openid': '112D1F37C43B195B0B95611CDC3DE5AE',
-    'access_token': '03923735251FF74FF795BD980325263E',
+    'access_token': 'A5F3F343C8646667B9A9B2CC383163C1',
     'orderBy': 'OverAll',
     'orientation': 'desc'
 }
 url = "https://nba2k2app.game.qq.com/game/trade/rosterList"
 r = requests.get(url, params=paras, verify=False)
+print(r.json())
 print(len(r.json()['data']['rosterList']))
 print(json.dumps(r.json(), indent=4, ensure_ascii=False))
 
 url = "https://nba2k2app.game.qq.com/rosterDetail?ids=4101&level=20&grade=7&mode=1"
-r = requests.get(url, params=paras, verify=False)
+# r = requests.get(url, params=paras, verify=False)
 # print(json.dumps(r.json(), indent=4, ensure_ascii=False))
